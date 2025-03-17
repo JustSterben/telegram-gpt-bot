b140ba61b33665e539526fb17551ecf546aa18cd
JustSterben <v2583280@gmail.com>
2025-03-17 21:00:15 +0300

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -109,7 +109,7 @@ def call_gate():
     headers = {"Content-Type": "application/json"}
     params = {
         "operation": "genCall",
-        "sipuid": SIPNET_LOGIN,
+        "login": SIPNET_LOGIN,
         "password": SIPNET_PASSWORD,
         "DstPhone": SHLAGBAUM_NUMBER,
         "format": "json"
